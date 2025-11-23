6edca0e236ba3ca7cf899bf73d6de149bae23561
fuharu <haruto7fujimoto@gmail.com>
2025-11-24 04:26:32 +0900

    fix: Remove invalid disableNormalPass prop from EffectComposer

--- src/components/features/Scene3D.tsx
+++ src/components/features/Scene3D.tsx
@@ -112,7 +112,7 @@ export default function Scene3D() {
                     <AbstractShape />
                     <Environment preset="city" blur={0.8} />
 
-                    <EffectComposer disableNormalPass>
+                    <EffectComposer>
                         <Bloom luminanceThreshold={1} mipmapBlur intensity={0.5} radius={0.4} />
                         <Noise opacity={0.02} />
                         <Vignette eskil={false} offset={0.1} darkness={0.5} />
